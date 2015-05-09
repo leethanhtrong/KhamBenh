97bfd773dc90751aa31b8d82ef870904d7f5a53d
Thành Trọng Lê <leethanhtrong603@gmail.com>
2015-05-09 11:38:54 +0700

    345

--- src/com/example/khambenh/ArrangementActivity.java
+++ src/com/example/khambenh/ArrangementActivity.java
@@ -3,26 +3,29 @@ package com.example.khambenh;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class ArrangementActivity extends Activity {
-	Boolean isInternetPresent = false;
-	ConnectionDetector cd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_arrangement);
-		isInternetPresent = cd.isConnectingToInternet();
-		if (!isInternetPresent) {
-			Toast.makeText(this, "Không có kết nối!", Toast.LENGTH_LONG).show();
-		} else {
+		ConnectivityManager cn = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo nf = cn.getActiveNetworkInfo();
+		if (nf != null && nf.isConnected() == true) {
 			DataFragment dataFragment = new DataFragment();
 			FragmentManager fragmentManager = getFragmentManager();
 			FragmentTransaction ft = fragmentManager.beginTransaction();
 			ft.replace(R.id.content_frame, dataFragment);
 			ft.commit();
+		} else {
+			Toast.makeText(ArrangementActivity.this, "Không có kết nối!",
+					Toast.LENGTH_LONG).show();
 		}
 	}
 
