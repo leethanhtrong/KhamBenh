023e5d6b485be863290356e09851e8bf7954738c
Đặng Liên Minh <uit11520232@gmail.com>
2015-05-25 22:03:49 +0700

    change dialog result

--- src/com/example/khambenh/ResultDialog.java
+++ src/com/example/khambenh/ResultDialog.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -29,21 +30,20 @@ public class ResultDialog extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		TextView myMsg = new TextView(getActivity());
-		myMsg.setText("Mã khám bệnh: "+makb+"\nHọ tên: " + name + "Thời gian hẹn: " + time
-				+ "Thông tin đăng ký của bạn đã được gửi về email" + email);
+		myMsg.setText("Mã khám bệnh: "+makb+"\nHọ tên: " + name + "\nThời gian hẹn: " + time
+				+ "\nThông tin đăng ký đã được gửi về email " + email+"\nChúc bạn một ngày tốt lành!");
 		myMsg.setGravity(Gravity.CENTER_VERTICAL);
 		return new AlertDialog.Builder(getActivity())
 				.setTitle("Đăng ký thành công").setMessage(myMsg.getText())
-				.setNegativeButton(android.R.string.no, new OnClickListener() {
+
+				.setPositiveButton(android.R.string.ok, new OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						// yes thì làm j
-					}
-				})
-				.setPositiveButton(android.R.string.yes, new OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						// cancel thì làm j
+						
+						//bạn có muốn đặt cuộc hẹn mới???
+						Intent i = new Intent(getActivity(),
+								ArrangementActivity.class);
+						startActivity(i);
 					}
 				}).create();
 	}
